d638135808cd5aded9625aaca10d7ab99b4c578a
rafael.fernandes <rafael.fernandes@toppen.com.br>
2019-07-11 11:38:51 -0300

    Include socket filenames

---
+++ merge_hapstat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+main_sock = '/var/run/haproxy/info.sock'
+proc_socks = [
+    '/var/run/haproxy/hastat-proc1.sock',
+    '/var/run/haproxy/hastat-proc2.sock',
+    '/var/run/haproxy/hastat-proc3.sock',
+    '/var/run/haproxy/hastat-proc4.sock',
+    '/var/run/haproxy/hastat-proc5.sock',
+]
