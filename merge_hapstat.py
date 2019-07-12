c0b7847b9a4d267730cdf3809f82ee7b604ab9f5
rafael.fernandes <rafael.fernandes@toppen.com.br>
2019-07-12 12:12:06 -0300

    HPSockets class, generate_csv function

--- merge_hapstat.py
+++ merge_hapstat.py
@@ -22,7 +22,8 @@ class Config():
             f'{self.sockets_dir}/'
             f'{conf.get("proc_socket_mask", "hastat-proc*.sock")}'
         )
-        self.enum_stats = dict(enumerate(conf.get('header'), 1))
+        self.header = conf.get('header')
+        self.enum_stats = dict(enumerate(self.header, 1))
         self.avg_list = [24, 25, 39, 58, 59, 60, 62,]
         self.fix_list = [1, 2, 7, 18, 19, 20, 21, 26, 28,
                         29, 30, 32, 33, 35, 37, 38, 56,]
@@ -32,16 +33,40 @@ class Config():
                         48, 49, 50, 51, 52, 53, 54, 55, 61,]
 
 
-def show_stat(sock):
-    if os.path.exists(sock):
-        client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        client.connect(sock)
-        client.send(b'show stat\n')
-        response = StringIO(client.recv(8192).decode('utf-8'))
-        client.close()
-        return csv.DictReader(response)
+class HPSockets(Config):
+    
+    def __init__(self):
+        if os.path.exists(self.merged_sock):
+            os.remove(self.merged_sock)
+
+        self.result = {}
+        self.procs_data = {
+            os.path.basename(sock):[] for sock in self.proc_sock_list
+        }
+
+    def show_stat(self, sock):
+        if os.path.exists(sock):
+            client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            client.connect(sock)
+            client.send(b'show stat\n')
+            response = StringIO(client.recv(8192).decode('utf-8'))
+            client.close()
+            return csv.DictReader(response)
+
+    def generate_csv(self, result_dict):
+        csv_response = []
+        csv_response.append(','.join(self.header))
+        for server in result_dict:
+            tmp_list = []
+            for stat in self.header:
+                tmp_list.append(str(result_dict[server][stat]))
+            csv_response.append(','.join(tmp_list))
+        return '\n'.join(csv_response)
+
 
 if __name__ == '__main__':
-    conf = Config()
-    procs_data = {os.path.basename(sock):[] for sock in conf.proc_sock_list}
-    results = {}
+    # conf = Config()
+    # procs_data = {os.path.basename(sock):{} for sock in conf.proc_sock_list}
+    manage = HPSockets()
+    
+
