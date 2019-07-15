b6b19cefc6b0a2eeafa123dd1573442c1678eaf4
rafael.fernandes <rafael.fernandes@toppen.com.br>
2019-07-15 08:59:44 -0300

    Reorganize functions

--- merge_hapstat.py
+++ merge_hapstat.py
@@ -40,24 +40,8 @@ class HPSockets(Config):
         self.procs_data = {}
         self.avg_count = {}
         self.server = self._create_unix_socket()
-        
-    def _create_unix_socket():
-        if os.path.exists(Config.merged_sock):
-            os.remove(Config.merged_sock)
-        
-        server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        server.bind(Config.merged_sock)
-        server.listen(5)
-        os.chmod(Config.merged_sock, 0o666)
-        return server
-    
-    def wait_unix_socket_request(self):
-        connection, client_address = server.accept()
-        data  = connection.recv(1024)
-        if data.startswith('show stat'):
-            connection.sendall(self.generate_csv() + '\n\n')
 
-    def update_proc_stats(self):
+    def _update_proc_stats(self):
         procs_data = {
             os.path.basename(sock):{} for sock in Config.proc_sock_list
         }
@@ -65,18 +49,9 @@ class HPSockets(Config):
             socket = os.path.basename(socket_path)
             updated_dict = self.run_show_stats(socket_path)
             procs_data[socket] = updated_dict
-        return procs_data
+        self.procs_data = procs_data
 
-    def run_show_stats(self, sock):
-        if os.path.exists(sock):
-            client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            client.connect(sock)
-            client.send(b'show stat\n')
-            response = StringIO(client.recv(8192).decode('utf-8'))
-            client.close()
-            return csv.DictReader(response)
-
-    def include_missing_stat(self, srv_num, stat_num):
+    def _include_missing_stat(self, srv_num, stat_num):
         if self.result.get(srv_num):
             if self.result[srv_num].get(stat_num):
                 return False
@@ -87,8 +62,8 @@ class HPSockets(Config):
             self.result[srv_num][stat_num] = ''
         return True
 
-    def merge_stat(self, srv_num, stat_num, value):
-        is_updated = include_missing_stat(srv_num, stat_num)
+    def _merge_stat(self, srv_num, stat_num, value):
+        is_updated = self._include_missing_stat(srv_num, stat_num)
         if is_updated:
             if stat_num in self.fix_list:
                 self.result[srv_num][stat_num] = value
@@ -107,11 +82,36 @@ class HPSockets(Config):
             if stat_num in self.avg_list:
                 self.avg_count[srv_num][stat_num] += 1          
 
-    def update_average(self):
+    def _update_average(self):
         for srv_num in self.result:
             for stat_num, count in self.avg_count.items():
                 average = self.result[srv_num][stat_num] / count
                 self.result[srv_num][stat_num] = average
+
+    def create_unix_socket():
+        if os.path.exists(Config.merged_sock):
+            os.remove(Config.merged_sock)
+        
+        server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        server.bind(Config.merged_sock)
+        server.listen(5)
+        os.chmod(Config.merged_sock, 0o666)
+        return server
+    
+    def wait_unix_socket_request(self):
+        connection, client_address = server.accept()
+        data  = connection.recv(1024)
+        if data.startswith('show stat'):
+            connection.sendall(self.generate_csv() + '\n\n')
+
+    def run_show_stats(self, sock):
+        if os.path.exists(sock):
+            client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            client.connect(sock)
+            client.send(b'show stat\n')
+            response = StringIO(client.recv(8192).decode('utf-8'))
+            client.close()
+            return csv.DictReader(response)
 
     def generate_csv(self):
         csv_response = []
@@ -124,7 +124,5 @@ class HPSockets(Config):
         return '\n'.join(csv_response)
 
 if __name__ == '__main__':
-    # conf = Config()
-    # procs_data = {os.path.basename(sock):{} for sock in conf.proc_sock_list}
     hpsock = HPSockets()
-    hpsock.procs_data = hpsock.update_proc_stats()
+    
